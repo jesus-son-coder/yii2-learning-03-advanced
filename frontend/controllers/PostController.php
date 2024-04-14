b552648419e3ff6311ad0cd221a4022d1c9645fd
jesus-son-coder <rvck2007@gmail.com>
2024-04-14 21:48:23 +0200

    Contraindre tous les endpoints Post à une authentification Bearer - v1

--- frontend/controllers/PostController.php
+++ frontend/controllers/PostController.php
@@ -4,9 +4,21 @@ namespace frontend\controllers;
 
 // use common\models\Post;
 use frontend\resource\Post;
+use yii\filters\auth\HttpBearerAuth;
 use yii\rest\ActiveController;
 
 class PostController extends ActiveController
 {
     public $modelClass = Post::class;
+
+    public function behaviors(): array
+    {
+        $behaviors = parent::behaviors();
+
+        $behaviors['authenticator'] = [
+            'class' => HttpBearerAuth::class
+        ];
+
+        return $behaviors;
+    }
 }
